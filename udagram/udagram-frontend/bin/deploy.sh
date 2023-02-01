f1cd604f0769c424831c0754d3037a022a59c13a
M0stafa123 <jodat.ahmed135@gmail.com>
2023-02-01 11:36:51 +0200

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
+aws s3 mb s3://myawsbucket589745
 aws s3 cp --recursive --acl public-read ./www s3://myawsbucket589745/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket589745/
